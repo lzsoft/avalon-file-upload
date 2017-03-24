4f5526ce431495c6bdce674d738e779bdb9ffce4
JingAn Chen <lzsoft.cja@gmail.com>
2017-03-23 20:31:49 -0400

    It's uploading correctly.

--- src/file-uploader.js
+++ src/file-uploader.js
@@ -6,7 +6,12 @@ window.customElements.define('file-uploader', class extends HTMLElement {
         let self = this;
         window.Lzsoft.Import.ByTagImport(self);
         self.querySelector("input").addEventListener("change", function() {
-            console.log(this.files);
+            window.Lzsoft.Api.Core(self.getAttribute("data-url"), {
+                requestContentType: this.files[0].type,
+                requestData: this.files[0]
+            }).then(function(result) {
+                console.log(result);
+            });
         });
         self.addEventListener("click", function() {
             self.querySelector("input").click();
