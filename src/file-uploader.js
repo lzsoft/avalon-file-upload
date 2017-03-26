ab9bf216cb2931d7d4e69a6323e4e8d90c0f975f
JingAn Chen <lzsoft.cja@gmail.com>
2017-03-26 15:35:34 -0400

    Now the file-uploader takes meta url and data-id/data-json attrubutes.
    When any of those changes and meta-url specified, the file-uploader will
    try to get meta info from API and render its background/get file info.

--- src/file-uploader.js
+++ src/file-uploader.js
@@ -1,6 +1,6 @@
 window.customElements.define('file-uploader', class extends HTMLElement {
     static get observedAttributes() {
-        return ['data-id'];
+        return ['data-json', 'data-id'];
     }
     constructor() {
         super();
@@ -9,18 +9,14 @@ window.customElements.define('file-uploader', class extends HTMLElement {
         let self = this;
         window.Lzsoft.Import.ByTagImport(self);
         self.querySelector("input").addEventListener("change", function() {
-            if (this.files[0]) {
-                window.Lzsoft.Api.Core(self.getAttribute("data-url"), {
+            if (this.files[0] && self.hasAttribute("data-upload-url")) {
+                window.Lzsoft.Api.Core(self.getAttribute("data-upload-url"), {
                     requestContentType: this.files[0].type,
                     requestData: this.files[0]
                 }).then(function(result) {
                     result.json().then(function(json) {
-                        let info = json || {};
-                        if (info.id) {
-                            self.setAttribute("data-id", json.id);
-                        }
-                        if (info.url && info.contentCategory === "image") {
-                            self.style.backgroundImage = `url(${info.url})`;
+                        if (json) {
+                            self.setAttribute("data-json", JSON.stringify(json));
                         }
                     }).catch(function(e) {
                         // Returning info is not a json
@@ -32,5 +28,42 @@ window.customElements.define('file-uploader', class extends HTMLElement {
             self.querySelector("input").click();
         });
     }
-    attributeChangedCallback(name, oldValue, newValue) {}
+    attributeChangedCallback(name, oldValue, newValue) {
+        let self = this;
+        switch (name) {
+            case 'data-id':
+                self.idChange(newValue);
+                break;
+            case 'data-json':
+                self.jsonChange(newValue);
+                break;
+        }
+    }
+    idChange(newValue) {
+        let self = this;
+        if (self.hasAttribute("data-meta-url")) {
+            window.Lzsoft.Api.Core(self.getAttribute("data-meta-url"), {
+                requestContentType: "application/json",
+                requestData: JSON.stringify({ id: newValue })
+            }).then(function(result) {
+                result.json().then(function(json) {
+                    if (json) {
+                        self.setAttribute("data-json", JSON.stringify(json));
+                    }
+                }).catch(function(e) {
+                    // Returning info is not a json
+                });
+            });
+        }
+    }
+    jsonChange(newValue) {
+        let self = this;
+        let json = JSON.parse(newValue || "{}");
+        if (!self.hasAttribute("data-id") && json.id) {
+            self.setAttribute("data-id", json.id);
+        }
+        if (json.url && json.contentCategory === "image") {
+            self.style.backgroundImage = `url(${json.url})`;
+        }
+    }
 });
