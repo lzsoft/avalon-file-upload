505a1ff97fd1241fb2b8097fb5f5cd2fd2059576
JingAn Chen <lzsoft.cja@gmail.com>
2017-03-26 15:02:46 -0400

    Render background depends on the returning contentCategory attribute.

--- src/file-uploader.js
+++ src/file-uploader.js
@@ -1,4 +1,7 @@
 window.customElements.define('file-uploader', class extends HTMLElement {
+    static get observedAttributes() {
+        return ['data-id'];
+    }
     constructor() {
         super();
     }
@@ -6,15 +9,28 @@ window.customElements.define('file-uploader', class extends HTMLElement {
         let self = this;
         window.Lzsoft.Import.ByTagImport(self);
         self.querySelector("input").addEventListener("change", function() {
-            window.Lzsoft.Api.Core(self.getAttribute("data-url"), {
-                requestContentType: this.files[0].type,
-                requestData: this.files[0]
-            }).then(function(result) {
-                console.log(result);
-            });
+            if (this.files[0]) {
+                window.Lzsoft.Api.Core(self.getAttribute("data-url"), {
+                    requestContentType: this.files[0].type,
+                    requestData: this.files[0]
+                }).then(function(result) {
+                    result.json().then(function(json) {
+                        let info = json || {};
+                        if (info.id) {
+                            self.setAttribute("data-id", json.id);
+                        }
+                        if (info.url && info.contentCategory === "image") {
+                            self.style.backgroundImage = `url(${info.url})`;
+                        }
+                    }).catch(function(e) {
+                        // Returning info is not a json
+                    });
+                });
+            }
         });
         self.addEventListener("click", function() {
             self.querySelector("input").click();
         });
     }
+    attributeChangedCallback(name, oldValue, newValue) {}
 });
