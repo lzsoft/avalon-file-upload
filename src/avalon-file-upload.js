07c6a4118cb70fc3d3c4fc79c3e5e4103f8cbac3
JingAn Chen <lzsoft.cja@gmail.com>
2017-04-18 23:44:10 -0400

    Use new AFEI camelCase interface.

--- src/avalon-file-upload.js
+++ src/avalon-file-upload.js
@@ -9,7 +9,7 @@ window.customElements.define('avalon-file-upload', class extends window.HTMLTinp
         let self = this;
         self.querySelector("input").addEventListener("change", async function() {
             if (this.files[0] && self.hasAttribute("data-url")) {
-                let json = await window.Lzsoft.Api.Put(self.getAttribute("data-url"), this.files[0]);
+                let json = await window.tingting.api.put(self.getAttribute("data-url"), this.files[0]);
                 if (json) {
                     self.setAttribute("data-id", json.id);
                 }
@@ -22,7 +22,7 @@ window.customElements.define('avalon-file-upload', class extends window.HTMLTinp
     async attributeChangedCallback(name, oldValue, newValue) {
         let self = this;
         if (self.hasAttribute("data-url") && newValue) {
-            let json = await window.Lzsoft.Api.Get(self.getAttribute("data-url"), { id: newValue });
+            let json = await window.tingting.api.get(self.getAttribute("data-url"), { id: newValue });
             if (json) {
                 if (json.url && json.contentCategory === "image") {
                     self.style.backgroundImage = `url(${json.url})`;
