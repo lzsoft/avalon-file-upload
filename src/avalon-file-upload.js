d4d1bc7508422fdf03bc899a50ff73609be3dbac
JingAn Chen <lzsoft.cja@gmail.com>
2017-08-01 11:03:16 -0400

    Remove the HTML Import structure.

--- src/avalon-file-upload.js
+++ src/avalon-file-upload.js
@@ -1,33 +1,36 @@
-window.customElements.define('avalon-file-upload', class extends window.HTMLTinplateElement {
-    static get observedAttributes() {
-        return ['data-id'];
-    }
-    constructor() {
-        super();
-    }
-    connectedCallback() {
-        let self = this;
-        self.querySelector("input").addEventListener("change", async function() {
-            if (this.files[0] && self.hasAttribute("data-url")) {
-                let json = await window.tingting.api.put(self.getAttribute("data-url"), this.files[0]);
-                if (json) {
-                    self.setAttribute("data-id", json.id);
+document.currentScript.loadSameNameHTML(function(template) {
+    window.customElements.define('avalon-file-upload', class extends window.HTMLTinplateElement {
+        static get observedAttributes() {
+            return ['data-id'];
+        }
+        constructor() {
+            super();
+        }
+        connectedCallback() {
+            this.innerHTML = template;
+            let self = this;
+            self.querySelector("input").addEventListener("change", async function() {
+                if (this.files[0] && self.hasAttribute("data-url")) {
+                    let json = await window.tingting.api.put(self.getAttribute("data-url"), this.files[0]);
+                    if (json) {
+                        self.setAttribute("data-id", json.id);
+                    }
                 }
-            }
-        });
-        self.addEventListener("click", function() {
-            self.querySelector("input").click();
-        });
-    }
-    async attributeChangedCallback(name, oldValue, newValue) {
-        let self = this;
-        if (self.hasAttribute("data-url") && newValue) {
-            let json = await window.tingting.api.get(self.getAttribute("data-url"), { id: newValue });
-            if (json) {
-                if (json.url && json.contentCategory === "image") {
-                    self.style.backgroundImage = `url(${json.url})`;
+            });
+            self.addEventListener("click", function() {
+                self.querySelector("input").click();
+            });
+        }
+        async attributeChangedCallback(name, oldValue, newValue) {
+            let self = this;
+            if (self.hasAttribute("data-url") && newValue) {
+                let json = await window.tingting.api.get(self.getAttribute("data-url"), { id: newValue });
+                if (json) {
+                    if (json.url && json.contentCategory === "image") {
+                        self.style.backgroundImage = `url(${json.url})`;
+                    }
                 }
             }
         }
-    }
+    });
 });
